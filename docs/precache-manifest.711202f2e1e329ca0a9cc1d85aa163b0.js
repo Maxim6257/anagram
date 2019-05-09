5cc79d95bc92adbe2d5adf93dffe817941c86f7a
Maxim <maxim.kohan6257@gmail.com>
2019-05-09 22:07:08 +0300

    build

---
+++ docs/precache-manifest.711202f2e1e329ca0a9cc1d85aa163b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341bd53f5ea270bb834710b9f8165488",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e1717f851fdca1c6961",
+    "url": "./static/css/main.17a03a3c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8e1717f851fdca1c6961",
+    "url": "./static/js/main.8687669b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
